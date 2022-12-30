95d80b6994d38bf5e26d124498a0d84215dd19db
yan <yan@chn.com>
2022-12-30 15:18:44 +0800

    v4版本

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -1,5 +1,8 @@
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
     }
 }
